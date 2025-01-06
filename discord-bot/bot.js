3a7037da17728bd20603b0b67055a0830bdc8981
SarCraft <nathan.henaux30@gmail.com>
2025-01-06 22:29:30 +0000

    🐛 Fix: pmmp save worlds

--- discord-bot/bot.js
+++ discord-bot/bot.js
@@ -4,7 +4,6 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 console.log(process.env.DISCORD_TOKEN);
